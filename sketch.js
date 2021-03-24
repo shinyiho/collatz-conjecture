afec6d15bef1678ed54622a57042ec3e08adfc9c
shinyiho <gbb2237@gmail.com>
2021-03-24 17:17:10 -0400

    make it animate

--- sketch.js
+++ sketch.js
@@ -1,44 +1,46 @@
+let arr = [];
+let ul = 1;
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight);
-	background(51);
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(51);
 
-	let len = 12;
-	let angle = PI / 16;
-	for (let i = 1; i <= 5000; i++) {
-		// translate(20, 22);
-		let arr = [];
-		let p = i;
-		do {
-			arr.push(p);
-			p = collatz(p);
-		} while (p !== 1);
-		arr.push(1);
-		arr.forEach((point) => {
-			if (point === 1) {
-				resetMatrix();
-				translate(width / 2, height / 2);
-			}
-			// translate(30, 30);
+  for (let i = 1; i <= 500000; i++) {
+    let p = i;
+    do {
+      arr.push(p);
+      p = collatz(p);
+    } while (p !== 1);
+    arr.push(1);
+  }
+}
 
-			if (point % 2 === 0) {
-				rotate(angle);
-			} else {
-				rotate(-angle);
-			}
-			strokeWeight(4);
-			stroke("rgba(255,204,0,0.1)");
+function draw() {
+  let len = 12;
+  let angle = PI / 16;
+  for (let j = ul - 300; j <= ul; j++) {
+    if (arr[j] === 1) {
+      resetMatrix();
+      translate(width / 2, height / 2);
+    }
+    if (arr[j] % 2 === 0) {
+      rotate(angle);
+    } else {
+      rotate(-angle);
+    }
+    strokeWeight(4);
+    stroke("rgba(255,204,0,0.1)");
 
-			line(0, 0, 0, -len);
-			text(point, 0, 0);
-			translate(0, -len);
-		});
-	}
+    line(0, 0, 0, -len);
+    text(arr[j], 0, 0);
+    translate(0, -len);
+  }
+  ul = ul + 300;
 }
 
 function collatz(n) {
-	if (n % 2 === 0) {
-		return n / 2;
-	} else {
-		return (3 * n + 1) / 2;
-	}
+  if (n % 2 === 0) {
+    return n / 2;
+  } else {
+    return (3 * n + 1) / 2;
+  }
 }
